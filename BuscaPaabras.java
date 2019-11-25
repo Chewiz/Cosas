003d6d9b9acbfb25cf20a4f6d88bc4a6b321be95
Chewiz <57515863+Chewiz@users.noreply.github.com>
2019-11-25 02:24:36 -0600

    Add files via upload

---
+++ BuscaPaabras.java
@@ -0,0 +1,31 @@
+    
+    /**
+     * Write a description of class BuscaPaabras here.
+     * 
+     * @author (your name) 
+     * @version (a version number or a date)
+     */
+    
+    import java.util.Scanner;
+    
+    public class BuscaPaabras
+    {
+        public static void main(String []args){
+            //
+            char sr =' ';
+            Scanner read = new Scanner(System.in);
+            String p[]={"Alla","Detras","Burro","Caldo","Error"};
+            System.out.println("Intruduzca una letra");
+            sr = read.next().charAt(0);
+            sr = Character.toLowerCase(sr);
+            for(int i = 0; i < p.length; i++){
+                if(Character.toLowerCase(p[i].charAt(0))== sr){
+                    System.out.println("La palabra es: "+p[i]);
+                    i = p.length;
+                }else{
+                    System.out.println("La palabra no se encuentra");
+                    i = p.length;
+                }
+            }
+        }
+    }
---
+++ NumOpten.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class NumOpten here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.Scanner;
+public class NumOpten
+{
+    //
+    public static void main(String[]args){
+        //
+        Scanner read = new Scanner(System.in);
+        int po=0, cont =0;
+        
+        String nu="";
+        String num="";
+        System.out.print("Ingrese un numero: ");
+        nu = read.nextLine();
+        char array[]= nu.toCharArray();
+        
+        System.out.print("Posicion del numero deseado: ");
+        po = read.nextInt();
+        int po1 = array.length-po;
+
+        for(int i =0; i<=po1; i++){
+            if(i == 0){
+                num+=array[po-1];
+            }else{
+                num+="0";
+            }
+            
+        }
+        System.out.println("El numero es: ");
+        System.out.println(num);
+    }
+}
